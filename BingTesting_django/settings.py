fa2907bb932b891d3734db305b12021c506cdf5e
libing <libingemail0104@163.com>
2021-03-25 19:52:46 +0800

    默认变更列表

--- BingTesting_django/settings.py
+++ BingTesting_django/settings.py
@@ -37,13 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "testweb"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'BingTesting_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+        # 'NAME': 'runoob', # 数据库名称
+        # 'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
+        # 'PORT': 3306, # 端口
+        # 'USER': 'root',  # 数据库用户名
+        # 'PASSWORD': '123456', # 数据库密码
     }
 }
 
--- testweb/views.py
+++ testweb/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import json
 # Create your views here.
+def index(request):
+ return HttpResponse(u"你好")
+#定义功能
+def add_args(a,b):
+    return a+b
+
+#接口函数
+def add(request):
+    if request.method == 'POST':
+        dic = {}
+        #判断是否传参
+        if request.POST:
+            a = request.POST.get('a',0)
+            b = request.POST.get('b',0)
+            #判断参数中是否含有a和b
+            if a and b :
+                res =  add_args(a,b)
+                dic['number'] = res
+                dic = json.dumps(dic)
+                return HttpResponse(dic)
+            else:
+                return HttpResponse('输入错误')
+        else:
+            return HttpResponse('输入为空')
+
+    else:
+        return HttpResponse('方法错误')
